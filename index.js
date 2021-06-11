75213786329f9eb3bfae9d529c6db4e952b172d3
Alexander Nguyen <41305327+nguyenalex214@users.noreply.github.com>
2021-06-11 12:53:50 -0700

    Final practice.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 //Can be used to display entire document variable to console. -AN
 //console.log(document);
 
@@ -43,3 +44,48 @@ const ratings = document.querySelectorAll(".rating-display .value");
 for (let element of ratings.values()) {
   console.log(element);
 }
+*/
+
+//Starting Updating the DOM Section. -AN
+//using innerHTML
+const descriptions = document.querySelectorAll(".description-display");
+for(let desc of descriptions.values()) {
+    let content = desc.innerText;
+    if(content.length > 250){
+        content = content.slice(0, 250);
+        content = content + '<a href="#">...</a>';
+    }
+    //console.log(content);
+    desc.innerHTML = content;
+}
+
+//Adding style with CSS high-rating and removing existing value.
+const ratings = document.querySelectorAll(".rating-display .value");
+for(let rating of ratings) {
+    let ratingValue = parseFloat(rating.innerText);
+    //console.log(ratingValue);
+    if(ratingValue > 4.7) {
+        rating.classList.add("high-rating");
+        rating.classList.remove("value");
+    }
+}
+
+//Creating a new element with text. -AN
+const parks = document.querySelectorAll(".park-display");
+const numberParks = parks.length;
+const newElement = document.createElement("div");
+newElement.innerText = `${numberParks} exciting parks to visit`;
+newElement.classList.add("header-statement");
+const header = document.querySelector("header");
+header.appendChild(newElement);
+
+//Removing DOM elements. -AN
+//Get the parent element of all parks.
+const main = document.querySelector("main");
+
+//Select a single park. 
+//const park = main.querySelector(".park-display");
+
+//Remove that park.
+//main.removeChild(park);
+
